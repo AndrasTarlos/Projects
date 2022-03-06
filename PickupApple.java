e6c5cf4e9bf99b30c815d8af9a3a4d502666b904
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-03-06 16:18:13 +0100

    Delete PickupApple.java

--- PickupApple.java
+++
@@ -1,8 +0,0 @@
-public class PickupApple extends Sound {
-    private final String fileName = currentRelativePath.toAbsolutePath().toString() + "\\src\\sounds\\pickup_apple.wav";
-
-    @Override
-    public void run() {
-        playSound(fileName);
-    }
-}
