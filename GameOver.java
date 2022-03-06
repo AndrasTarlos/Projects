6fd94f83904b967cf1de57eb312b8ce3f44ebb6f
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-03-06 16:17:52 +0100

    Delete GameOver.java

--- GameOver.java
+++
@@ -1,8 +0,0 @@
-public class GameOver extends Music {
-    private final String fileName = currentRelativePath.toAbsolutePath().toString() + "\\src\\sounds\\game_over.wav";
-
-    @Override
-    public void run() {
-        playSound(fileName);
-    }
-}
