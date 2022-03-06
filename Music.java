7db2ccc277f4c7aa17744d62cc6bc2d2b4ec83dc
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-03-06 16:18:03 +0100

    Delete Music.java

--- Music.java
+++
@@ -1,8 +0,0 @@
-public class Music extends Sound {
-    private final String fileName = currentRelativePath.toAbsolutePath().toString() + "\\src\\sounds\\fat_rat.wav";
-
-    @Override
-    public void run() {
-        playMusic(fileName);
-    }
-}
