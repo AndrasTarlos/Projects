65f1e655d01879a3ca5c72b8c5b0cf37157dda5c
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-08-22 17:44:21 +0100

    Add files via upload

---
+++ Websites/medox36_group_project/js/main.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+    mybutton = document.getElementById("to_top");
+    scrollbar = document.getElementById("progress_part");
+}
+
+window.onscroll = function() {scrollFunction(); scrollBar()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none"
+    }
+}
+
+function toTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function scrollBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
